3eb760cfef5b1047a9be0586a5bb8fec92600144
Thomas Wolff <thomas.wolff@nrk.no>
2021-04-08 19:44:45 +0200

    Oppdaterte DTO

--- src/api/Dto.fs
+++ src/api/Dto.fs
@@ -1,12 +1,12 @@
 module NRK.Dotnetskolen.Dto
 
-open System
-
 type Sending = {
     Tittel: string
-    Kanal: string
     StartTidspunkt: string
     SluttTidspunkt: string
 }
 
-type Epg = Sending list
+type Epg = {
+  NRK1: Sending list
+  NRK2: Sending list
+}
