825fc827d6bdf2d2b093c37cc085a199b0e5dfef
Andreas Thomassen <andreas.thomassen@nrk.no>
2021-04-28 13:37:30 +0200

    More testing

--- src/api/Domain.cs
+++ src/api/Domain.cs
@@ -1,7 +1,6 @@
 using System;
-using System.Linq;
 using System.Collections.Generic;
-
+using System.Text.RegularExpressions;
 
 namespace NRK.Dotnetskolen
 {
@@ -11,6 +10,28 @@ namespace NRK.Dotnetskolen
         public string Kanal { get; set; }
         public DateTimeOffset StartTidspunkt { get; set; }
         public DateTimeOffset SluttTidspunkt { get; set; }
+
+        public static bool IsTitleValid(string title)
+        {
+            Regex titleRegex = new (@"^[\p{L}0-9\.,-:!]{5,100}$");
+            return titleRegex.IsMatch(title);
+        }
+
+        public static bool IsChannelValid(string channel)
+        {
+            Regex channelRegex = new(@"NRK1|NRK2");
+            return channelRegex.IsMatch(channel);
+        }
+
+        public static bool IsStartAndEndTimeIsValid(DateTimeOffset startTime, DateTimeOffset endTime)
+        {
+            return endTime > startTime;
+        }
+
+        public static bool IsTransmissionValid(Sending transmission)
+        {
+            return IsTitleValid(transmission.Tittel) && IsChannelValid(transmission.Kanal) && IsStartAndEndTimeIsValid(transmission.StartTidspunkt, transmission.SluttTidspunkt);
+        }
     }
 
     public class Epg
--- test/unit/UnitTests.cs
+++ test/unit/UnitTests.cs
@@ -1,13 +1,58 @@
+using System;
 using Xunit;
 
 namespace NRK.Dotnetskolen.UnitTests
 {
     public class UnitTests
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("abc12")]
+        [InlineData(".,-:!")]
+        [InlineData("ABCDEFGHIJABCDEFGHIJABCDEFGHIJABCDEFGHIJABCDEFGHIJABCDEFGHIJABCDEFGHIJABCDEFGHIJABCDEFGHIJABCDEFGHIJ")]
+        public void IsTitleValid_ValidTitle_ReturnsTrue(string tittel)
         {
+            Assert.True(Sending.IsTitleValid(tittel));
+        }
 
+        [Theory]
+        [InlineData("abcd")]
+        [InlineData("@$%&/")]
+        [InlineData("abcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghija")]
+        public void IsTitleValid_InvalidTitle_ReturnsFalse(string title)
+        {
+            Assert.False(Sending.IsTitleValid(title));
+        }
+
+        [Theory]
+        [InlineData("NRK1")]
+        [InlineData("NRK2")]
+        public void IsChannelValid_ValidChannel_ReturnsTrue(string channel)
+        {
+            Assert.True(Sending.IsChannelValid(channel));
+        }
+
+        [Theory]
+        [InlineData("nrk1")]
+        [InlineData("NRK3")]
+        public void IsChannelValid_InvalidChannel_ReturnsFalse(string channel)
+        {
+            Assert.False(Sending.IsChannelValid(channel));
+        }
+
+        [Fact]
+        public void AreStartAndEndTimesValid_StartBeforeEnd_ReturnsTrue()
+        {
+            DateTimeOffset startTime = DateTimeOffset.Now;
+
+            Assert.True(Sending.IsStartAndEndTimeIsValid(startTime, startTime.AddMinutes(30)));
+        }
+
+        [Theory]
+        [InlineData("Urix", "NRK1", "2021-04-29T22:00:00+02", "2021-04-29T22:45:00+02", false)]
+        [InlineData("Kveldsnytt", "NRK1", "2021-04-29T22:00:00+02", "2021-04-29T22:45:00+02", true)]
+        public void IsTransmissionValid_Sending_VaryingOutput(string title, string channel, string start, string end, bool valid)
+        {
+            Assert.Equal(valid, Sending.IsTransmissionValid(new Sending { Tittel = title, Kanal = channel, StartTidspunkt = DateTimeOffset.Parse(start), SluttTidspunkt = DateTimeOffset.Parse(end) }));
         }
     }
 }
