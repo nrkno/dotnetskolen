5653f4168525b9f3d25853e20c17779e49d42430
Ola Toft <ola.toft@nrk.no>
2021-04-29 10:07:52 +0200

    Definer domenemodell

---
+++ src/api/Domain.fs
@@ -0,0 +1,13 @@
+namespace NRK.Dotnetskolen
+
+module Domain =
+    open System
+
+    type Sending = {
+        Tittel: string
+        Kanal: string
+        StartTidspunkt: DateTimeOffset
+        SluttTidspunkt: DateTimeOffset
+    }
+
+    type Epg = Sending list
--- src/api/Program.fs
+++ src/api/Program.fs
@@ -1,13 +1,16 @@
-// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
-
 open System
-
-// Define a function to construct a message to print
-let from whom =
-    sprintf "from %s" whom
+open NRK.Dotnetskolen.Domain
 
 [<EntryPoint>]
 let main argv =
-    let message = from "F#" // Call the function
-    printfn "Hello world %s" message
-    0 // return an integer exit code
+    let epg = [
+        {
+            Tittel = "Dagsrevyen"
+            Kanal = "NRK1"
+            StartTidspunkt = DateTimeOffset.Parse("2021-04-16T19:00:00+02:00")
+            SluttTidspunkt = DateTimeOffset.Parse("2021-04-16T19:30:00+02:00")
+        }
+    ]
+
+    printfn "%A" epg
+    0
