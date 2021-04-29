1b593b2d6562cbb70d2f9c8b2f6847326357ac39
Ola Toft <ola.toft@nrk.no>
2021-04-29 11:15:50 +0200

    Legg til validering og einingstestar for domenemodell

--- src/api/Domain.fs
+++ src/api/Domain.fs
@@ -2,6 +2,7 @@ namespace NRK.Dotnetskolen
 
 module Domain =
     open System
+    open System.Text.RegularExpressions
 
     type Sending = {
         Tittel: string
@@ -11,3 +12,28 @@ module Domain =
     }
 
     type Epg = Sending list
+
+    let isTitleValid (title: string) : bool =
+        let titleRegex = Regex(@"^[\p{L}0-9\.,-:!]{5,100}$")
+        titleRegex.IsMatch(title)
+
+    let isChannelValid (channel: string) : bool =
+        match channel with
+        | "NRK1"
+        | "NRK2" -> true
+        | _ -> false
+
+
+    let areStartAndEndTimesValid (startTime: DateTimeOffset) (endTime: DateTimeOffset) =
+        startTime < endTime
+
+    let isTransmissionValid (transmission: Sending) : bool =
+        let isTitleValid = isTitleValid transmission.Tittel
+        let isChannelValid = isChannelValid transmission.Kanal
+
+        let areStartAndEndTimesValid =
+            areStartAndEndTimesValid transmission.StartTidspunkt transmission.SluttTidspunkt
+
+        match isTitleValid, isChannelValid, areStartAndEndTimesValid with
+        | true, true, true -> true
+        | _ -> false
--- test/unit/Tests.fs
+++ test/unit/Tests.fs
@@ -2,7 +2,89 @@ module Tests
 
 open System
 open Xunit
+open NRK.Dotnetskolen.Domain
+
+[<Theory>]
+[<InlineData("abc12")>]
+[<InlineData(".,-:!")>]
+[<InlineData("ABCDEFGHIJABCDEFGHIJABCDEFGHIJABCDEFGHIJABCDEFGHIJABCDEFGHIJABCDEFGHIJABCDEFGHIJABCDEFGHIJABCDEFGHIJ")>]
+let ``isTitleValid valid title returns true`` (title: string) =
+    let isTitleValid = isTitleValid title
+
+    Assert.True isTitleValid
+
+[<Theory>]
+[<InlineData("abcd")>]
+[<InlineData("@$%&/")>]
+[<InlineData("abcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghija")>]
+let ``isTitleValid invalid title returns false`` (title: string) =
+    let isTitleValid = isTitleValid title
+
+    Assert.False isTitleValid
+
+[<Theory>]
+[<InlineData("NRK1")>]
+[<InlineData("NRK2")>]
+let ``isChannelValid valid channel returns true`` (channel: string) =
+    let isChannelValid = isChannelValid channel
+
+    Assert.True isChannelValid
+
+[<Theory>]
+[<InlineData("nrk1")>]
+[<InlineData("NRK3")>]
+let ``isChannelvalid invalid channel returns false`` (channel: string) =
+    let isChannelValid = isChannelValid channel
+
+    Assert.False isChannelValid
 
 [<Fact>]
-let ``My test`` () =
-    Assert.True(true)
+let ``areStartAndEndTimesValid start before end returns true`` () =
+    let startTime = DateTimeOffset.Now
+    let endTime = startTime.AddMinutes 30.
+
+    let areStartAndEndTimesValid = areStartAndEndTimesValid startTime endTime
+
+    Assert.True areStartAndEndTimesValid
+
+[<Fact>]
+let ``areStartAndEndTimesValid start after end returns false`` () =
+    let endTime = DateTimeOffset.Now
+    let startTime = endTime.AddMinutes 30.
+
+    let areStartAndEndTimesValid = areStartAndEndTimesValid startTime endTime
+
+    Assert.False areStartAndEndTimesValid
+
+[<Fact>]
+let ``areStartAndEndTimesValid start at the same time as end returns false`` () =
+    let startTime = DateTimeOffset.Now
+    let endTime = startTime
+
+    let areStartAndEndTimesValid = areStartAndEndTimesValid startTime endTime
+
+    Assert.False areStartAndEndTimesValid
+
+[<Fact>]
+let ``isTransmissionValid valid transmission returns true`` () =
+    let transmission = {
+        Tittel = "Dagsrevyen"
+        Kanal = "NRK1"
+        StartTidspunkt = DateTimeOffset.Parse("2021-04-16T19:00:00+02:00")
+        SluttTidspunkt = DateTimeOffset.Parse("2021-04-16T19:30:00+02:00")
+    }
+
+    let isTransmissionValid = isTransmissionValid transmission
+    Assert.True isTransmissionValid
+
+[<Fact>]
+let ``isTransmissionValid transmission with invalid channel returns false`` () =
+    let transmission = {
+        Tittel = "Dagsrevyen"
+        Kanal = "NRK3"
+        StartTidspunkt = DateTimeOffset.Parse("2021-04-16T19:00:00+02:00")
+        SluttTidspunkt = DateTimeOffset.Parse("2021-04-16T19:30:00+02:00")
+    }
+
+    let isTransmissionValid = isTransmissionValid transmission
+    Assert.False isTransmissionValid
