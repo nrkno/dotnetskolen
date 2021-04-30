ed8476b4928abc0f6c9d0a6ac378606a6f31493c
Thomas Wolff <thomas.wolff@nrk.no>
2021-04-30 09:37:23 +0200

    async

--- test/integration/Tests.fs
+++ test/integration/Tests.fs
@@ -18,40 +18,43 @@ let createWebHostBuilder () =
         .ConfigureServices(Program.configureServices)
 
 [<Fact>]
-let ``Get EPG today returns 200 OK`` () =
+let ``Get EPG today returns 200 OK`` () = async {
     use testServer = new TestServer(createWebHostBuilder())
     use client = testServer.CreateClient()
     let todayAsString = DateTimeOffset.Now.ToString "yyyy-MM-dd"
     let url = sprintf "/epg/%s" todayAsString
 
-    let response = client.GetAsync(url) |> Async.AwaitTask |> Async.RunSynchronously
+    let! response = client.GetAsync(url) |> Async.AwaitTask
 
     response.EnsureSuccessStatusCode() |> ignore
+}
     
 [<Fact>]
-let ``Get EPG today return valid response`` () =
+let ``Get EPG today return valid response`` () = async {
     use testServer = new TestServer(createWebHostBuilder())
     use client = testServer.CreateClient()
     let todayAsString = DateTimeOffset.Now.ToString "yyyy-MM-dd"
     let url = sprintf "/epg/%s" todayAsString
     let jsonSchema = JsonSchema.FromFile "./epg.schema.json"
 
-    let response = client.GetAsync(url) |> Async.AwaitTask |> Async.RunSynchronously
+    let! response = client.GetAsync(url) |> Async.AwaitTask
 
     response.EnsureSuccessStatusCode() |> ignore
-    let bodyAsString = response.Content.ReadAsStringAsync() |> Async.AwaitTask |> Async.RunSynchronously
+    let! bodyAsString = response.Content.ReadAsStringAsync() |> Async.AwaitTask
     let bodyAsJsonDocument = JsonDocument.Parse(bodyAsString).RootElement
     let isJsonValid = jsonSchema.Validate(bodyAsJsonDocument, ValidationOptions(RequireFormatValidation = true)).IsValid
     
     Assert.True(isJsonValid)
+}
 
 [<Fact>]
-let ``Get EPG invalid date returns bad request`` () =
+let ``Get EPG invalid date returns bad request`` () = async {
     use testServer = new TestServer(createWebHostBuilder())
     use client = testServer.CreateClient()
     let invalidDateAsString = "2021-13-32"
     let url = sprintf "/epg/%s" invalidDateAsString
 
-    let response = client.GetAsync(url) |> Async.AwaitTask |> Async.RunSynchronously
+    let! response = client.GetAsync(url) |> Async.AwaitTask
 
     Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode)
+}
