5f249ece687dae7858fd2405b191da3ad45c2476
Ola Toft <ola.toft@nrk.no>
2021-04-30 15:24:21 +0200

    Steg 9: Legg til integrasjonstestar for web-API
    Testane passerer ikkje endå

--- src/api/Program.fs
+++ src/api/Program.fs
@@ -1,16 +1,29 @@
-open System
-open NRK.Dotnetskolen.Domain
+namespace NRK.Dotnetskolen.Api
 
-[<EntryPoint>]
-let main argv =
-    let epg = [
-        {
-            Tittel = "Dagsrevyen"
-            Kanal = "NRK1"
-            StartTidspunkt = DateTimeOffset.Parse("2021-04-16T19:00:00+02:00")
-            SluttTidspunkt = DateTimeOffset.Parse("2021-04-16T19:30:00+02:00")
-        }
-    ]
+module Program =
+    open System
+    open NRK.Dotnetskolen.Domain
 
-    printfn "%A" epg
-    0
+    open Microsoft.AspNetCore.Hosting
+    open Microsoft.Extensions.DependencyInjection
+    open Microsoft.AspNetCore.Builder
+
+    let configureApp (webHostContext: WebHostBuilderContext) (app: IApplicationBuilder) =
+        ()
+
+    let configureServices (webHostContext: WebHostBuilderContext) (services: IServiceCollection) =
+        ()
+
+    [<EntryPoint>]
+    let main argv =
+        let epg = [
+            {
+                Tittel = "Dagsrevyen"
+                Kanal = "NRK1"
+                StartTidspunkt = DateTimeOffset.Parse("2021-04-16T19:00:00+02:00")
+                SluttTidspunkt = DateTimeOffset.Parse("2021-04-16T19:30:00+02:00")
+            }
+        ]
+
+        printfn "%A" epg
+        0
--- test/integration/Tests.fs
+++ test/integration/Tests.fs
@@ -1,8 +1,76 @@
 module Tests
 
 open System
+open System.IO
+open System.Net
+open Microsoft.AspNetCore.TestHost
+open Microsoft.AspNetCore.Hosting
 open Xunit
+open NRK.Dotnetskolen.Api
+open Json.Schema
+open System.Text.Json
+
+let createWebHostBuilder () =
+    WebHostBuilder()
+        .UseContentRoot(Directory.GetCurrentDirectory())
+        .UseEnvironment("Test")
+        .Configure(Program.configureApp)
+        .ConfigureServices(Program.configureServices)
 
 [<Fact>]
-let ``My test`` () =
-    Assert.True(true)
+let ``Get EPG today returns 200 OK`` () =
+    use testServer = new TestServer(createWebHostBuilder())
+    use client = testServer.CreateClient()
+    let todayAsString = DateTimeOffset.Now.ToString "yyyy-MM-dd"
+    let url = sprintf "/epg/%s" todayAsString
+
+    let response =
+        client.GetAsync(url)
+        |> Async.AwaitTask
+        |> Async.RunSynchronously
+
+    response.EnsureSuccessStatusCode()
+    |> ignore
+
+[<Fact>]
+let ``Get EPG today returns valid response`` () =
+    use testServer = new TestServer(createWebHostBuilder())
+    use client = testServer.CreateClient()
+    let todayAsString = DateTimeOffset.Now.ToString "yyyy-MM-dd"
+    let url = sprintf "/epg/%s" todayAsString
+    let jsonSchema = JsonSchema.FromFile "./epg.schema.json"
+
+    let response =
+        client.GetAsync(url)
+        |> Async.AwaitTask
+        |> Async.RunSynchronously
+
+    response.EnsureSuccessStatusCode()
+    |> ignore
+
+    let bodyAsString =
+        response.Content.ReadAsStringAsync()
+        |> Async.AwaitTask
+        |> Async.RunSynchronously
+
+    let bodyAsJsonDocument = JsonDocument.Parse(bodyAsString).RootElement
+
+    let isJsonValid =
+        jsonSchema
+            .Validate(bodyAsJsonDocument, ValidationOptions(RequireFormatValidation = true)).IsValid
+
+    Assert.True(isJsonValid)
+
+[<Fact>]
+let ``Get EPG invalid date returns bad request`` () =
+    use testServer = new TestServer(createWebHostBuilder())
+    use client = testServer.CreateClient()
+    let invalidDateAsString = "2021-13-32"
+    let url = sprintf "/epg/%s" invalidDateAsString
+
+    let response =
+        client.GetAsync(url)
+        |> Async.AwaitTask
+        |> Async.RunSynchronously
+
+    Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode)
