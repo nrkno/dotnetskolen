d82febe72838fc9810c48a94636787e035cab1c6
Thomas Wolff <thomaswolff90@gmail.com>
2021-05-02 11:44:46 +0200

    Endring i oppbygging av veiledning

--- src/api/Program.fs
+++ src/api/Program.fs
@@ -2,28 +2,28 @@ namespace NRK.Dotnetskolen.Api
 
 module Program = 
 
-    open System
-    open NRK.Dotnetskolen.Domain
-
+    open Microsoft.Extensions.Hosting
     open Microsoft.AspNetCore.Hosting
-    open Microsoft.Extensions.DependencyInjection
     open Microsoft.AspNetCore.Builder
+    open Microsoft.Extensions.DependencyInjection
+    open Giraffe
 
     let configureApp (webHostContext: WebHostBuilderContext) (app: IApplicationBuilder) =
-        ()
+        let webApp = route "/ping" >=> text "pong"
+        app.UseGiraffe webApp
 
     let configureServices (webHostContext: WebHostBuilderContext) (services: IServiceCollection) =
-        ()
+        services.AddGiraffe() |> ignore
+
+    let createHostBuilder args =
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(fun webHostBuilder ->
+                webHostBuilder
+                    .Configure(configureApp)
+                    .ConfigureServices(configureServices) |> ignore
+            )
 
     [<EntryPoint>]
     let main argv =
-        let epg = [
-            {
-                Tittel = "Dagsrevyen"
-                Kanal = "NRK1"
-                StartTidspunkt = DateTimeOffset.Parse("2021-04-16T19:00:00+02:00")
-                SluttTidspunkt = DateTimeOffset.Parse("2021-04-16T19:30:00+02:00")
-            }
-        ]
-        printfn "%A" epg
-        0 // return an integer exit code
+        createHostBuilder(argv).Build().Run()
+        0
--- test/integration/Tests.fs
+++ test/integration/Tests.fs
@@ -1,13 +1,9 @@
 module Tests
 
-open System
 open System.IO
-open System.Net
-open System.Text.Json
 open Microsoft.AspNetCore.Hosting
 open Microsoft.AspNetCore.TestHost
 open Xunit
-open Json.Schema
 open NRK.Dotnetskolen.Api
 
 let createWebHostBuilder () =
@@ -18,43 +14,12 @@ let createWebHostBuilder () =
         .ConfigureServices(Program.configureServices)
 
 [<Fact>]
-let ``Get EPG today returns 200 OK`` () = async {
+let ``Get ping returns 200 OK`` () = async {
     use testServer = new TestServer(createWebHostBuilder())
     use client = testServer.CreateClient()
-    let todayAsString = DateTimeOffset.Now.ToString "yyyy-MM-dd"
-    let url = sprintf "/epg/%s" todayAsString
+    let url = "/ping"
 
     let! response = client.GetAsync(url) |> Async.AwaitTask
 
     response.EnsureSuccessStatusCode() |> ignore
-}
-    
-[<Fact>]
-let ``Get EPG today return valid response`` () = async {
-    use testServer = new TestServer(createWebHostBuilder())
-    use client = testServer.CreateClient()
-    let todayAsString = DateTimeOffset.Now.ToString "yyyy-MM-dd"
-    let url = sprintf "/epg/%s" todayAsString
-    let jsonSchema = JsonSchema.FromFile "./epg.schema.json"
-
-    let! response = client.GetAsync(url) |> Async.AwaitTask
-
-    response.EnsureSuccessStatusCode() |> ignore
-    let! bodyAsString = response.Content.ReadAsStringAsync() |> Async.AwaitTask
-    let bodyAsJsonDocument = JsonDocument.Parse(bodyAsString).RootElement
-    let isJsonValid = jsonSchema.Validate(bodyAsJsonDocument, ValidationOptions(RequireFormatValidation = true)).IsValid
-    
-    Assert.True(isJsonValid)
-}
-
-[<Fact>]
-let ``Get EPG invalid date returns bad request`` () = async {
-    use testServer = new TestServer(createWebHostBuilder())
-    use client = testServer.CreateClient()
-    let invalidDateAsString = "2021-13-32"
-    let url = sprintf "/epg/%s" invalidDateAsString
-
-    let! response = client.GetAsync(url) |> Async.AwaitTask
-
-    Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode)
 }
