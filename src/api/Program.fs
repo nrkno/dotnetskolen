f04bce2bed924cf4b5c8200216c38e0cfb950b9a
Thomas Wolff <thomas.wolff@nrk.no>
2022-06-15 14:29:20 +0200

    Forenklinger for å bygge opp veiledning stegvis

--- src/api/Program.fs
+++ src/api/Program.fs
@@ -2,22 +2,9 @@
 
 module Program = 
 
-    open System
     open Microsoft.AspNetCore.Builder
-
-    let createBuilder (argv: string []) =
-        let builder = WebApplication.CreateBuilder(argv)
-        // Do stuff with builder here
-        builder
-
-    let createApp (builder: WebApplicationBuilder) =
-        let app = builder.Build()
-        app.MapGet("/", Func<string>(fun () -> "Hello World!")) |> ignore
-        app
 
     [<EntryPoint>]
     let main argv =
-        let builder = createBuilder argv
-        let app = createApp builder
-        app.Run()
+        WebApplication.CreateBuilder(argv).Build().Run()
         0
--- test/integration/Tests.fs
+++ test/integration/Tests.fs
@@ -1,27 +1,8 @@
 module Tests
 
-open System.Net.Http
+open System
 open Xunit
-open Microsoft.AspNetCore.TestHost
-open NRK.Dotnetskolen.Api.Program
-
-let runWithTestClient (test: HttpClient -> Async<unit>) = 
-    async {
-        let builder = createBuilder([||])
-        builder.WebHost.UseTestServer() |> ignore
-
-        use app = createApp builder
-        do! app.StartAsync() |> Async.AwaitTask
-
-        let client = app.GetTestClient()
-        do! test client
-    } |> Async.RunSynchronously
 
 [<Fact>]
-let ``A request to "/" returns "Hello World!"`` () =
-    runWithTestClient (fun httpClient -> 
-        async {
-            let! helloWorld = httpClient.GetStringAsync("/") |> Async.AwaitTask
-            Assert.Equal(helloWorld, "Hello World!")
-        }
-    )
+let ``My test`` () =
+    Assert.True(true)
