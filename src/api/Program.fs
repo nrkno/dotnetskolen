c3380957f4a339703a6002acc30a07a5930c9ec5
Thomas Wolff <thomas.wolff@nrk.no>
2022-06-15 16:02:20 +0200

    Ferdig implementasjon for steg 9

--- src/api/Program.fs
+++ src/api/Program.fs
@@ -2,9 +2,20 @@
 
 module Program = 
 
+    open System
     open Microsoft.AspNetCore.Builder
+
+    let createWebApplicationBuilder (argv: string[]) =
+        WebApplication.CreateBuilder(argv)
+
+    let createWebApplication (builder: WebApplicationBuilder) =
+        let app = builder.Build()
+        app.MapGet("ping", Func<string>(fun () -> "pong")) |> ignore
+        app
 
     [<EntryPoint>]
     let main argv =
-        WebApplication.CreateBuilder(argv).Build().Run()
+        let builder = createWebApplicationBuilder argv
+        let app = createWebApplication builder
+        app.Run()
         0
--- test/integration/Tests.fs
+++ test/integration/Tests.fs
@@ -1,8 +1,27 @@
 module Tests
 
-open System
+open System.Net.Http
 open Xunit
+open Microsoft.AspNetCore.TestHost
+open NRK.Dotnetskolen.Api.Program
+
+let runWithTestClient (test: HttpClient -> Async<unit>) = 
+    async {
+        let builder = createWebApplicationBuilder([||])
+        builder.WebHost.UseTestServer() |> ignore
+
+        use app = createWebApplication builder
+        do! app.StartAsync() |> Async.AwaitTask
+
+        let testClient = app.GetTestClient()
+        do! test testClient
+    } |> Async.RunSynchronously
 
 [<Fact>]
-let ``My test`` () =
-    Assert.True(true)
+let ``Get "ping" returns "pong"`` () =
+    runWithTestClient (fun httpClient -> 
+        async {
+            let! response = httpClient.GetStringAsync("ping") |> Async.AwaitTask
+            Assert.Equal(response, "pong")
+        }
+    )
