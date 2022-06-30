eee82fda00180f87f7edaae0b714ecbfe601395d
Thomas Wolff <thomas.wolff@nrk.no>
2022-06-30 14:26:11 +0200

    async -> task

--- test/integration/Tests.fs
+++ test/integration/Tests.fs
@@ -1,27 +1,28 @@
 module Tests
 
 open System.Net.Http
+open System.Threading.Tasks
 open Xunit
 open Microsoft.AspNetCore.TestHost
 open NRK.Dotnetskolen.Api.Program
 
-let runWithTestClient (test: HttpClient -> Async<unit>) = 
-    async {
+let runWithTestClient (test: HttpClient -> Task<unit>) = 
+    task {
         let builder = createWebApplicationBuilder()
         builder.WebHost.UseTestServer() |> ignore
 
         use app = createWebApplication builder
-        do! app.StartAsync() |> Async.AwaitTask
+        do! app.StartAsync()
 
         let testClient = app.GetTestClient()
         do! test testClient
-    } |> Async.RunSynchronously
+    }
 
 [<Fact>]
 let ``Get "ping" returns "pong"`` () =
     runWithTestClient (fun httpClient -> 
-        async {
-            let! response = httpClient.GetStringAsync("ping") |> Async.AwaitTask
+        task {
+            let! response = httpClient.GetStringAsync("ping")
             Assert.Equal(response, "pong")
         }
     )
