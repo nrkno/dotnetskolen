ef532c1a464cac8ea59347f2dd502b50083a5c75
Thomas Wolff <thomas.wolff@nrk.no>
2022-06-30 14:42:18 +0200

    DateTime -> DateTimeOffset

--- src/api/Program.fs
+++ src/api/Program.fs
@@ -13,7 +13,7 @@ module Program =
     let createWebApplicationBuilder () =
         WebApplication.CreateBuilder()
 
-    let createWebApplication (builder: WebApplicationBuilder) (getEpgForDate: DateTime -> Epg) =
+    let createWebApplication (builder: WebApplicationBuilder) (getEpgForDate: DateTimeOffset -> Epg) =
         let app = builder.Build()
         app.MapGet("/ping", Func<string>(fun () -> "pong")) |> ignore
         app.MapGet("/epg/{date}", Func<string, IResult>(fun date -> epgHandler getEpgForDate date)) |> ignore
