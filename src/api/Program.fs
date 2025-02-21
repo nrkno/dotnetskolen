5c0a0c5461ebfdfcb16a07feb934fc856932f9ea
Heidi Mork <heidi.mork@nrk.no>
2025-02-21 15:43:42 +0100

    step 8: outlining the web API

--- src/api/Program.fs
+++ src/api/Program.fs
@@ -1,12 +1,18 @@
-﻿open System
-open NRK.Dotnetskolen.Domain
+﻿namespace NRK.Dotnetskolen.Api
 
-let epg = [
-    {
-        Title = "Dagsrevyen"
-        Channel = "NRK1"
-        StartTime = DateTimeOffset.Parse("2021-04-16T19:00:00+02:00")
-        EndTime = DateTimeOffset.Parse("2021-04-16T19:30:00+02:00")
-    }
-]
-printfn "%A" epg
+module Program =
+
+    open System
+    open Microsoft.AspNetCore.Builder
+
+    let createWebApplicationBuilder () =
+        WebApplication.CreateBuilder()
+
+    let createWebApplication (builder: WebApplicationBuilder) =
+        let app = builder.Build()
+        app.MapGet("/ping", Func<string>(fun () -> "pong")) |> ignore
+        app
+
+    let builder = createWebApplicationBuilder()
+    let app = createWebApplication builder
+    app.Run()
--- test/integration/Tests.fs
+++ test/integration/Tests.fs
@@ -1,8 +1,28 @@
 ﻿module Tests
 
-open System
+open System.Net.Http
+open System.Threading.Tasks
 open Xunit
+open Microsoft.AspNetCore.TestHost
+open NRK.Dotnetskolen.Api.Program
+
+let runWithTestClient (test: HttpClient -> Task<unit>) =
+    task {
+        let builder = createWebApplicationBuilder()
+        builder.WebHost.UseTestServer() |> ignore
+
+        use app = createWebApplication builder
+        do! app.StartAsync()
+
+        let testClient = app.GetTestClient()
+        do! test testClient
+    }
 
 [<Fact>]
-let ``My test`` () =
-    Assert.True(true)
+let ``Get "ping" returns "pong"`` () =
+    runWithTestClient (fun httpClient ->
+        task {
+            let! response = httpClient.GetStringAsync("ping")
+            Assert.Equal(response, "pong")
+        }
+    )
